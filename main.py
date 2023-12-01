bae07a06afff575a19cb1accefca3d6ff1072127
giornogiovannya <teklik7@gmail.com>
2023-12-01 21:26:30 +0300

    save uploads to folder

--- main.py
+++ main.py
@@ -12,5 +12,19 @@ def ping():
     return jsonify(response)
 
 
+@app.route('/send_photo', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+
+    if file:
+        file.save('/path/to/save/' + file.filename)
+        return jsonify({'message': 'File uploaded successfully'})
+
+
 if __name__ == '__main__':
     app.run(debug=True)
