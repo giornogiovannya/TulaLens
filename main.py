9477bb2bb4324c39ffa2305efb182bff83ce38d3
giornogiovannya <teklik7@gmail.com>
2023-12-02 08:47:57 +0300

    fix path

--- main.py
+++ main.py
@@ -26,5 +26,18 @@ def upload_file():
         return jsonify({'message': 'File uploaded successfully'})
 
 
+@app.route('/upload_photos', methods=['POST'])
+def upload_photos():
+    if 'photo0' not in request.files:
+        return jsonify({'error': 'No photos part'})
+
+    photos = request.files.getlist('photo0')
+
+    for idx, photo in enumerate(photos):
+        photo.save(f'/home/aboba/static/photo_{idx}.jpg')
+
+    return jsonify({'message': 'Photos uploaded successfully'})
+
+
 if __name__ == '__main__':
     app.run(debug=True)
